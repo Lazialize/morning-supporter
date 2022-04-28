2f2c53c1a22ac403c5a18976687c27fd1c29cc42
NaokiTakahashi <ntakahashi@landscape.co.jp>
2022-04-28 11:39:52 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'morning-supporter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
