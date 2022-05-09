a712e20b2e3a9cddcd07ca0f108979dc5fd035df
Lazialize <lazinelia@gmail.com>
2022-05-09 04:37:52 +0000

    feat: Add modules for authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,26 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, initializeAuth } from '@angular/fire/auth';
+import { AuthGuard } from '@angular/fire/auth-guard';
+
+import { indexedDBLocalPersistence } from 'firebase/auth';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => initializeAuth(getApp(), { persistence: indexedDBLocalPersistence })),
+  ],
+  providers: [AuthGuard, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyAedow-fGdiJ7ILDKcCKgXtI1YvhK49__I',
+    authDomain: 'morning-supporter.firebaseapp.com',
+    projectId: 'morning-supporter',
+    storageBucket: 'morning-supporter.appspot.com',
+    messagingSenderId: '416177372589',
+    appId: '1:416177372589:web:2c07e82aad5a06c45d3524',
+  },
 };
 
 /*
