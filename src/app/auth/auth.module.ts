0568bf15e1fe5c34d1c3593332a1a033b363f6f3
Lazialize <lazinelia@gmail.com>
2022-05-09 17:13:30 +0900

    feat: Implement a page to sign in and sign-up
    You can access firebase currently. You are on fire. Ouch!

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupPage } from './signup/signup.page';
+import { SigninPage } from './signin/signin.page';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupPage,
+  },
+  {
+    path: 'signin',
+    component: SigninPage,
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [SignupPage, SigninPage],
+  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes)],
 })
 export class AuthModule {}
