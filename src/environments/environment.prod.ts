c211b69de4c805a5a2f34e62d4693b414b17dade
Lazialize <lazinelia@gmail.com>
2022-05-12 18:26:16 +0900

    chore: Add the firebase settings to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true,
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAedow-fGdiJ7ILDKcCKgXtI1YvhK49__I',
+    authDomain: 'morning-supporter.firebaseapp.com',
+    projectId: 'morning-supporter',
+    storageBucket: 'morning-supporter.appspot.com',
+    messagingSenderId: '416177372589',
+    appId: '1:416177372589:web:2c07e82aad5a06c45d3524',
+  },
 };
