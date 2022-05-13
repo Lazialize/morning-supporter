2efcbbe6f3438c58a3e0fd65a2bbcaacebf79070
Lazialize <lazinelia@gmail.com>
2022-05-13 13:24:04 +0900

    feat: Implement a service
    To get the weather info in the current coordinates in real-time.

---
+++ src/app/shared/weather/service/weather.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { GeolocationService } from '@ng-web-apis/geolocation';
+import { Observable, Subject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { WeatherInfo } from '../types';
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService extends Observable<WeatherInfo> {
+  constructor(private http: HttpClient, private readonly geolocation$: GeolocationService) {
+    super();
+    const subject = new Subject<WeatherInfo>();
+    this.geolocation$.subscribe(
+      (position) =>
+        this.fetchWetherInformation(position.coords.latitude, position.coords.longitude)
+          .pipe(shareReplay({ bufferSize: 1, refCount: true }))
+          .subscribe((weatherInfo) => subject.next(weatherInfo)),
+      (err) => {
+        // Returns the weather info of the Tokyo if an error occurred in getting the current coordinates.
+        // TODO: It is a provisional implementation, so I will re-implement it to get the weather info by the set upped coordinates.
+        this.fetchWetherInformation(36, 140)
+          .pipe(shareReplay({ bufferSize: 1, refCount: true }))
+          .subscribe((weatherInfo) => subject.next(weatherInfo));
+      },
+    );
+    return subject.asObservable() as WeatherService;
+  }
+
+  private fetchWetherInformation(lat: number, lon: number, limit: number = 1) {
+    return this.http.get<WeatherInfo>(baseUrl, {
+      params: {
+        lat,
+        lon,
+        limit,
+        units: 'metric',
+        exclude: ['current', 'minutely', 'hourly', 'alerts'].join(','),
+        appid: environment.openWether.apiKey,
+      },
+    });
+  }
+}
---
+++ src/app/shared/weather/types.ts
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface DayWether {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  weather: Weather[];
+  clouds: number;
+  pop: number;
+  rain: number;
+  uvi: number;
+}
+
+export interface WeatherInfo {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  daily: DayWether[];
+}
