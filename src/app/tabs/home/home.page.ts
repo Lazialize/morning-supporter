0022f020da178bf6a333770b965626545cc085ec
Lazialize <lazinelia@gmail.com>
2022-05-13 17:13:21 +0900

    feat: Implement a feature
    To display the progress of the tasks.

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FirestoreService } from 'src/app/shared/firestore/firestore.service';
+import { ITaskWithId } from 'src/app/shared/firestore/types';
+import { WeatherService } from 'src/app/shared/weather/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  constructor() {}
+  tasks$: Observable<ITaskWithId[]>;
+  done: number;
+  total: number;
+  uncompletedTasks: ITaskWithId[];
 
-  ngOnInit() {}
+  constructor(readonly weatherInfo$: WeatherService, private firestore: FirestoreService) {}
+
+  ngOnInit() {
+    this.weatherInfo$.subscribe((weatherInfo) => console.log(weatherInfo));
+  }
+
+  ionViewWillEnter() {
+    this.tasks$ = this.firestore.fetchAllTask();
+    this.tasks$.subscribe((tasks) => {
+      this.done = 0;
+      this.total = 0;
+      this.uncompletedTasks = [];
+      for (const task of tasks) {
+        if (task.isDone) {
+          this.done++;
+        } else {
+          this.uncompletedTasks.push(task);
+        }
+        this.total++;
+      }
+    });
+  }
 }
