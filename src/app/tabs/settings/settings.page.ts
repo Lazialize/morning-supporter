b7fd021a368faaa89d73e647822882d1ffe14712
Lazialize <lazinelia@gmail.com>
2022-05-16 14:43:40 +0900

    feat: Implement a feature to set position and log out

--- src/app/tabs/settings/settings.page.ts
+++ src/app/tabs/settings/settings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { GeolocationPage } from 'src/app/shared/pages/geolocation/geolocation.page';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-  constructor() {}
+  constructor(private auth: AuthService, private modalController: ModalController) {}
 
   ngOnInit() {}
+
+  async openModal() {
+    this.modalController
+      .create({
+        component: GeolocationPage,
+      })
+      .then((modal) => modal.present());
+  }
+
+  signOut() {
+    this.auth.authSignOut();
+  }
 }
