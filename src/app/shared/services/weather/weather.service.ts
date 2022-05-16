1a8a2fa95eed09fa2d1aa63f09ae48c97cab74ff
Lazialize <lazinelia@gmail.com>
2022-05-16 14:23:57 +0900

    feat: Implement a feature to set location manually

--- src/app/shared/services/weather/weather.service.ts
+++ src/app/shared/services/weather/weather.service.ts
@@ -6,6 +6,8 @@ import { Observable, Subject } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { GeolocationPage } from '../../pages/geolocation/geolocation.page';
+import { AuthService } from '../auth/auth.service';
+import { FirestoreService } from '../firestore/firestore.service';
 import { WeatherInfo } from './types';
 
 const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall';
@@ -18,6 +20,8 @@ export class WeatherService extends Observable<WeatherInfo> {
     private http: HttpClient,
     private readonly geolocation$: GeolocationService,
     private modalController: ModalController,
+    private firestore: FirestoreService,
+    private auth: AuthService,
   ) {
     super();
     const subject = new Subject<WeatherInfo>();
@@ -26,17 +30,17 @@ export class WeatherService extends Observable<WeatherInfo> {
         this.fetchWetherInformation(position.coords.latitude, position.coords.longitude)
           .pipe(shareReplay({ bufferSize: 1, refCount: true }))
           .subscribe((weatherInfo) => subject.next(weatherInfo)),
-      (err) => {
-        // Returns the weather info of the Tokyo if an error occurred in getting the current coordinates.
-        // TODO: It is a provisional implementation, so I will re-implement it to get the weather info by the set upped coordinates.
-        modalController
-          .create({
-            component: GeolocationPage,
-          })
-          .then((modal) => modal.present());
-        this.fetchWetherInformation(36, 140)
-          .pipe(shareReplay({ bufferSize: 1, refCount: true }))
-          .subscribe((weatherInfo) => subject.next(weatherInfo));
+      async () => {
+        this.firestore.getUserSettingsById(this.auth.getUserId()).subscribe(async (settings) => {
+          if (!settings) {
+            await (await modalController.create({ component: GeolocationPage })).present();
+            return;
+          }
+
+          this.fetchWetherInformation(+settings.location.lat, +settings.location.lon)
+            .pipe(shareReplay({ bufferSize: 1, refCount: true }))
+            .subscribe((weatherInfo) => subject.next(weatherInfo));
+        });
       },
     );
     return subject.asObservable() as WeatherService;
