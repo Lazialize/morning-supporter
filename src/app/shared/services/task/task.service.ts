94c51a226fbbc5bf2b91c3e34f9512b6bada9719
Lazialize <lazinelia@gmail.com>
2022-05-27 17:42:44 +0900

    chore: for keeping data

--- src/app/shared/services/task/task.service.ts
+++ src/app/shared/services/task/task.service.ts
@@ -12,34 +12,89 @@ import {
   updateDoc,
   where,
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
+import { WeatherInfo } from '../weather/types';
+import { ConditionType, IDayOfWeekCondition } from './Interfaces/condition';
 import { IPartialTask, ITask, ITaskUpdateData, ITaskWithId } from './Interfaces/task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
+  private subscription: Subscription;
   private observer$: Observable<ITaskWithId[]>;
+  private subject: Subject<ITaskWithId[]>;
   private taskCollection: CollectionReference<ITask>;
+  private actualTasks: ITaskWithId[];
 
   private userId: string;
 
   constructor(private firestore: Firestore) {
     this.taskCollection = collection(this.firestore, 'tasks') as CollectionReference<ITask>;
+    this.subject = new Subject<ITaskWithId[]>();
     this.observer$ = null;
+    this.subscription = null;
   }
 
   initialize(userId: string): void {
+    if (this.subscription !== null) {
+      this.subscription.unsubscribe();
+    }
+
     this.userId = userId;
     this.observer$ = collectionData(
       query(this.taskCollection, orderBy('timestamp', 'asc'), where('uid', '==', userId)),
       { idField: 'id' },
     ) as Observable<ITaskWithId[]>;
+
+    this.observer$.subscribe((tasks) => {});
+
+    this.actualTasks = [];
+    this.subject.next(this.actualTasks);
   }
 
   getObserver(): Observable<ITaskWithId[]> {
-    return this.observer$;
+    return this.subject.asObservable();
+  }
+
+  updateTasksConditions(weatherInfo: WeatherInfo) {
+    this.observer$.pipe(first()).forEach((tasks) => {
+      const newTaskArray: ITaskWithId[] = [];
+      for (const task of tasks) {
+        if (!task.isTemporary) {
+          newTaskArray.push(task);
+          continue;
+        }
+
+        if (task.temporaryCondition.conditionType === ConditionType.dayOfWeek) {
+          const now = new Date(Date.now());
+          const condition = task.temporaryCondition.conditions as IDayOfWeekCondition;
+          if (condition.isOrdinal) {
+            const dayOfWeek = condition.dayOfWeeks as {
+              ordinal: number;
+              dayOfWeek: number;
+            };
+            if (now.getDay() !== dayOfWeek.dayOfWeek) {
+              continue;
+            }
+
+            if (Math.floor(now.getDate() / 7) + 1 !== dayOfWeek.ordinal) {
+              continue;
+            }
+            newTaskArray.push(task);
+          } else {
+            const dayOfWeek = condition.dayOfWeeks as number[];
+
+            if (dayOfWeek.includes(now.getDay())) {
+              newTaskArray.push(task);
+            }
+          }
+        }
+      }
+
+      this.subject.next(newTaskArray);
+    });
   }
 
   addTask(task: IPartialTask, isTemporary = false) {
